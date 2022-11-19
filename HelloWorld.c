05633eac80eece9b54b6a99c23a6dbf6cd439df3
Ham126 <RY7GAMES@GMAIL.COM>
2022-11-19 17:41:17 -0600

    HelloWorld.c : file added

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  printf ("+==================================+\n");
+  printf ("|                                  |\n");
+  printf ("|                                  |\n");
+  printf ("|     HELLO WORLD....              |\n");
+  printf ("|                                  |\n");
+  printf ("|                                  |\n");
+  printf ("+==================================+\n");
+}
